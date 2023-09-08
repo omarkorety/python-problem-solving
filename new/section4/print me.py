d271ee0b5c1d89c0fb43081b6b1e29075949da70
omarkorety <omarkorety@hotmail.com>
2023-09-08 15:49:00 +0300

    add new problems

---
+++ new/section4/print me.py
@@ -0,0 +1,5 @@
+A,B= map(int,input().split())
+if B == -1:
+    print(A*2+1)
+elif B == 1:
+    print(A*A)
---
+++ new/section4/swap _3_nums.py
@@ -0,0 +1,6 @@
+num1,num2,num3= map(int,input().split())
+temp=num1
+num1=num2
+num2=num3
+num3=temp
+print(num1,num2,num3)
